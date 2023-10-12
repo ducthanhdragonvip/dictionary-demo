1c3c2650b4282ee6caf687d00fc776e8c1c8b243
thanh <ducthanhdragonvip@gmail.com>
2023-10-13 01:50:56 +0700

    update UI

--- src/controller/mainAppController.java
+++ src/controller/mainAppController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -10,12 +12,48 @@ import main.DictionaryApp;
 
 import javafx.event.ActionEvent;
 
-public class mainAppController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class mainAppController implements Initializable {
     @FXML
     AnchorPane mainPane;
-
+    AnchorPane searchPane;
+    AnchorPane translatePane;
     @FXML
     Button buttonGoBack;
+
+    @FXML
+    Button btnSearch;
+    @FXML
+    Button translateButton;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            searchPane = FXMLLoader.load(getClass().getResource("/view/searchScene.fxml"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        try {
+            translatePane = FXMLLoader.load(getClass().getResource("/view/translatePane.fxml"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        setMainPane(searchPane);
+    }
+
+    public void setMainPane(AnchorPane pane) {
+        mainPane.getChildren().clear();
+        mainPane.getChildren().addAll(pane);
+    }
+
+    public void setBtnSearch (ActionEvent event) {
+        setMainPane(searchPane);
+    }
+
+    public void btnParagraphTranslate(ActionEvent event) {
+        setMainPane(translatePane);
+    }
 
     public void setButtonGoBack(ActionEvent event) {
         Stage getStage =(Stage) mainPane.getScene().getWindow();
