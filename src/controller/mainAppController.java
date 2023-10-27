11f2bf6b8bb5f7baa57b030b72fc955732c6269c
thanh <ducthanhdragonvip@gmail.com>
2023-10-28 02:00:09 +0700

    update UI and UX

--- src/controller/mainAppController.java
+++ src/controller/mainAppController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import main.DictionaryApp;
@@ -20,13 +19,25 @@ public class mainAppController implements Initializable {
     AnchorPane mainPane;
     AnchorPane searchPane;
     AnchorPane translatePane;
+
     @FXML
     Button buttonGoBack;
-
     @FXML
     Button btnSearch;
     @FXML
     Button translateButton;
+    @FXML
+    Button btnHome;
+    @FXML
+    Button btnNote;
+    @FXML
+    Button btnGame;
+    @FXML
+    Button btnAdd;
+    @FXML
+    Button btnConfig;
+
+
 
     public void initialize(URL location, ResourceBundle resources) {
         try {
@@ -42,18 +53,64 @@ public class mainAppController implements Initializable {
         setMainPane(searchPane);
     }
 
+    public void btnFocus(Button button) {
+        button.setStyle("-fx-background-color: linear-gradient(to right,#b4caff,#72c5ff, #7ee9ff);");
+    }
+
+    public void btnDefault() {
+        buttonGoBack.setStyle("-fx-background-color: #E0FFFF;-fx-background-radius: 0 20 20 0;");
+        btnSearch.setStyle("-fx-background-color: #E0FFFF;-fx-background-radius: 0 20 20 0;");
+        btnAdd.setStyle("-fx-background-color: #E0FFFF;-fx-background-radius: 0 20 20 0;");
+        btnGame.setStyle("-fx-background-color: #E0FFFF;-fx-background-radius: 0 20 20 0;");
+        btnNote.setStyle("-fx-background-color: #E0FFFF;-fx-background-radius: 0 20 20 0;");
+        btnHome.setStyle("-fx-background-color: #E0FFFF;-fx-background-radius: 0 20 20 0;");
+        translateButton.setStyle("-fx-background-color: #E0FFFF;-fx-background-radius: 0 20 20 0;");
+        btnConfig.setStyle("-fx-background-color: #E0FFFF;-fx-background-radius: 0 20 20 0;");
+    }
+
     public void setMainPane(AnchorPane pane) {
         mainPane.getChildren().clear();
         mainPane.getChildren().addAll(pane);
     }
 
+    public void setBtnHome(ActionEvent event) {
+        btnDefault();
+        btnFocus(btnHome);
+    }
+
     public void setBtnSearch (ActionEvent event) {
+        btnDefault();
+        btnFocus(btnSearch);
         setMainPane(searchPane);
     }
 
     public void btnParagraphTranslate(ActionEvent event) {
+        btnDefault();
+        btnFocus(translateButton);
         setMainPane(translatePane);
     }
+
+    public void setBtnNote(ActionEvent event) {
+        btnDefault();
+        btnFocus(btnNote);
+    }
+
+    public void setBtnGame(ActionEvent event) {
+        btnDefault();
+        btnFocus(btnGame);
+    }
+
+    public void setBtnAdd(ActionEvent event) {
+        btnDefault();
+        btnFocus(btnAdd);
+    }
+
+    public void setBtnConfig(ActionEvent event) {
+        btnDefault();
+        btnFocus(btnConfig);
+    }
+
+
 
     public void setButtonGoBack(ActionEvent event) {
         Stage getStage =(Stage) mainPane.getScene().getWindow();
