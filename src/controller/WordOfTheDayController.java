c1c55ce39d3a39f135759faff30bbc486723adaa
Napplee <hobacnguyen2004@gmail.com>
2023-11-30 12:27:26 +0700

    word of day upd

--- src/controller/WordOfTheDayController.java
+++ src/controller/WordOfTheDayController.java
@@ -2,6 +2,7 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -15,11 +16,12 @@ public class WordOfTheDayController {
     private Label pronunciationLabel;
     @FXML
     private Label partOfSpeechLabel;
-
-    @FXML
-    private Label exampleLable;
     @FXML
     private Label meaningLabel;
+    @FXML
+    private Label Example;
+    @FXML
+    private TextArea contextLabel;
     @FXML
     private Label dayLabel;
     @FXML
@@ -41,21 +43,32 @@ public class WordOfTheDayController {
             // The word on the website
             Element wordElement = doc.select("div.word-and-pronunciation h2").first();
             // Phiên âm
-
             Element pronunciationElement = doc.select("div.word-attributes span.word-syllables").first();
             //Noun, adjectives, verb
             Element partOfSpeechElement = doc.select("div.word-attributes span.main-attr").first();
 
-            Element exampleLabelElement =  doc.select("body > div.outer-container > div > div.main-wrapper.clearfix > main > article > div.lr-cols-area.clearfix.sticky-column.d-flex > div.left-content > div > div.wod-definition-container > p:nth-child(3)").first();
+
 
             Element meaningElement = doc.select("div.wod-definition-container p:first-of-type").first();
+
+            Element exampleElement = doc.select("div.wod-definition-container p").get(1);
+
+            Element contextElement = doc.select("div.wod-definition-container p:first-of-type").get(1);
 
             // Update your labels here
             wordLabel.setText(wordElement.text());
             pronunciationLabel.setText(pronunciationElement.text());
             partOfSpeechLabel.setText(partOfSpeechElement.text());
-            exampleLable.setText(exampleLabelElement.text());
             meaningLabel.setText(meaningElement.text());
+            contextLabel.setText(contextElement.text());
+
+            if (exampleElement != null) {
+                String exampleText = exampleElement.text();
+                Example.setText(exampleText);
+            } else {
+                // Xử lý trường hợp không tìm thấy ví dụ
+                Example.setText("Can not find example.");
+            }
         } catch (Exception e) {
             e.printStackTrace();
             // Handle exceptions appropriately
